f77eedf074873aee45a43ab982ebf0d5569d8f1f
Bassem Tarek <bassemtarekmahrous@gmail.com>
2025-04-15 04:00:14 +0000

    add @vercel/analytics/react

--- src/main.jsx
+++ src/main.jsx
@@ -13,5 +13,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
                 <App />
             </Suspense>
         </MemoryRouter>
+        <Analytics />
     </React.StrictMode>
 );
